a8e7409c67e368c13f2854e5cb7e679cfbecaa2b
KeplerInCoding <shreyakv8@gmail.com>
2023-11-17 22:10:40 +0530

    sm

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({post}) => {
   return (
     <div className='w-5/6 lg:w-3/6 p-4 m-2 rounded-lg shadow-sm bg-gradient-to-t from-slate-50 shadow-black border border-black'>
         <div className=' text-xl underline mb-2 font-extrabold'>{post.title}</div>
-        <div>By <span className=' italic'>{post.author}</span>on <span className='font-bold'>{post.category}</span> </div>
+        <div className='text-sm'>By <span className=' italic'>{post.author}</span>on <span className='font-bold'>{post.category}</span> </div>
         <div className='text-violet-700 text-sm'>Posted on {post.date}</div>
         <div className='my-3'>{post.content}</div>
         <div className='text-blue-500 text-sm underline flex flex-wrap gap-x-2'>{post.tags.map((tag)=>(<span>{`#${tag}`}</span>))}</div>
