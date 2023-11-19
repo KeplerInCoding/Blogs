9c033cf73261007a2b0e7e358ae1a1274bf8ef1d
KeplerInCoding <shreyakv8@gmail.com>
2023-11-19 11:46:09 +0530

    Category page done

--- src/Pages/TagPage.js
+++ src/Pages/TagPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import { useNavigation, useLocation } from 'react-router-dom';
+import Blogs from '..components/Blogs';
+import Pagination from '../components/Pagination';
+import Header from '../components/Header';
 
 const TagPage = () => {
   const navigation = useNavigation();
+  const location = useLocation();
+  const tag = location.pathname.split('/').at(-1);
   return (
     <div>
       <Header/>
@@ -12,7 +17,7 @@ const TagPage = () => {
           Back
         </button>
         <h2>
-          Blogs on <span>{category}</span>
+          Blogs Tagged <span>#{tag}</span>
         </h2>
       </div>
       <Blogs/>
